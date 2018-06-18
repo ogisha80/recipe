954a74098a63a4069c0b8cdb832c2e8c5713b3a1
Ognjen Krkelic <ognjen@MacBook-Pro.local>
2018-06-18 09:38:22 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/recipe/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package guru.springframework.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/" ,"/recipe"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
